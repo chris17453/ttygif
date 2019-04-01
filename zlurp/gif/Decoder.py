5bde42bcfc92216468c9b80f34cdf111202da201
Charles Watkins <chris17453@gmail.com>
2019-04-01 13:43:34 -0400

    zlurp

--- zlurp/gif/Decoder.py
+++ zlurp/gif/Decoder.py
@@ -1,202 +1,181 @@
-# Code converted from c code at 
-# http://enchantia.com/software/graphapp/package/src/libgif/gif.c
+from .stream import DataStream
+from .Header import Header
+from .ImageDescriptor import ImageDescriptor
+from .ImageData import ImageData
+from .GraphicsControlExtension import GraphicsControlExtension
+from .ApplicationExtension import ApplicationExtension
+from .CommentExtension import CommentExtension
+from .PlainTextExtension import PlainTextExtension
+from .Trailer import Trailer
+from .ColorTable import ColorTable
 
+# GIT 89A
+# freehand implimentation of information found at  https://www.fileformat.info/format/gif/egff.htm
+# Charles Watkins
 
 class Decoder:
-    def __init__(self,stream,depth):
-        self.LZ_MAX_CODE     =4095    # Largest 12 bit code
-        self.LZ_BITS         =12
-        self.FLUSH_OUTPUT    =4096    # Impossible code = flush
-        self.FIRST_CODE      =4097    # Impossible code = first
-        self.NO_SUCH_CODE    =4098    # Impossible code = empty
-        self.HT_SIZE         =8192    # 13 bit hash table size
-        self.HT_KEY_MASK     =0x1FFF  # 13 bit key mask
-        self.IMAGE_LOADING   =0       # file_state = processing
-        self.IMAGE_SAVING    =0       # file_state = processing
-        self.IMAGE_COMPLETE  =1       # finished reading or writing
+    #################################3
+    # header
+    #  - logical screen descriptor
+    # global color table
+    # comment extension
+    # application extension
+    # graphic control extension
+    # local image descriptor
+    # local color table
+    # image data
+    # comment extension
+    # plain test extension
+    # trailer
 
+    # 	PixelAspectRatio = (AspectRatio + 15) / 64;
+    # NumberOfGlobalColorTableEntries = 	 (1L << (SizeOfTheGlobalColorTable + 1));
 
-        self.position     = 0
-        self.bufsize      = 0
-        self.buf[0]       = 0
-        self.depth        = depth
-        self.clear_code   = (1 << depth)
-        self.eof_code     = self.clear_code + 1
-        self.running_code = self.eof_code + 1
-        self.running_bits = depth + 1
-        self.max_code_plus_one = 1 << self.running_bits
-        self.stack_ptr    = 0
-        self.prev_code    = NO_SUCH_CODE
-        self.shift_state  = 0
-        self.shift_data   = 0
-
-        # init prefix list
-        self.prefix=[self.NO_SUCH_CODE]*self.LZ_MAX_CODE
-
-    def read_gif_code():
-        code_masks=[
-            0x0000, 0x0001, 0x0003, 0x0007,
-            0x000f, 0x001f, 0x003f, 0x007f,
-            0x00ff, 0x01ff, 0x03ff, 0x07ff,
-            0x0fff ]
+    def __init__(self,file=None):
+        self.file=file
+        self.stream=DataStream(file)
         
-        while (self.shift_state < self.running_bits){
-            next_byte =stream.byte()
-            self.shift_data =self.shift_data  | next_byte << self.shift_state
-            self.shift_state += 8
-        }
-
-        code = self.shift_data & code_masks[self.running_bits]
-
-        self.shift_data  =self.shift_data  >> self.running_bits
-        self.shift_state =self.shift_state - self.running_bits
-
-        self.running_code+=1
-        if self.running_code > self.max_code_plus_one and self.running_bits < self.LZ_BITS:
-            self.max_code_plus_one=self.max_code_plus_one << 1
-            self.running_bits+=1
-        return code
-
-    def read_gif_line(self,length):
-        i = 0
-        line=[]
-        while (i < length):
-            current_code = self.read_gif_code()
-
-            if current_code == self.eof_code:
-                if (i != length - 1 or self.pixel_count != 0:
-                    raise Exception ("Unexpected EOF")
-                i+=1;
-            }
-            elif current_code == self.clear_code:
-                for j in range(0,self.LZ_MAX_CODE)
-                    self.prefix[j]     = self.NO_SUCH_CODE
-                self.running_code      = self.eof_code + 1
-                self.running_bits      = self.depth + 1
-                self.max_code_plus_one = 1 << self.running_bits
-                prev_code               = self.NO_SUCH_CODE
-            else:
-                if current_code < self.clear_code:
-                    line.append(current_code)
-                    i+=1
-                else :
-                    if current_code < 0 or current_code > self.LZ_MAX_CODE:
-                        raise Exception ("Image Defect")
-                    if (prefix[current_code] == NO_SUCH_CODE) {
-                        if (current_code == self.running_code - 2) {
-                            current_prefix = prev_code
-                            #?
-                            #suffix[self.running_code - 2]= stack[stack_ptr++] = trace_prefix(prefix, prev_code, clear_code)
+        self.stream.open()
+        self.header=Header(self.stream)
+        self.header.debug()
+        self.comments=[]
+        self.frames=[]
+        self.applications=[]
+        if self.header.GlobalColorTableFlag==True:
+            self.global_color_table=self.load_color_table(self.header.NumberOfGlobalColorTableEntries)
+        else:
+            # TODO default global color table
+            x=1
+        
+        loop=True
+        frame=0
+        old_pos=-1
+        while loop:
+            # try for an image
+            gc=self.load_graphics_control_extension()
+            if gc:
+                descriptor =self.load_image_descriptor()
+                if descriptor:
+                    if descriptor.LocalColorTableFlag==True:
+                        local_color_table=self.load_color_table(descriptor.NumberOfGlobalColorTableEntries)
                     else:
-                        raise Exception ("Image Defect")
-                else:
-                    current_prefix = current_code;
+                        local_color_table=None
+                    imagedata=self.load_image_data()
+                    self.frames.append({'frame':frame,'type':'image','gc':gc,'descriptor':descriptor,'color_table':local_color_table,'image':imagedata})
+                    frame+=1
+                continue
+            
+            # try for extensions
+            comment    =self.load_comment_extension()
+            if comment:
+                self.comments.append(comment)
+                continue
 
-                while (j++ <= self.LZ_MAX_CODE \
-                    and current_prefix >self. clear_code \
-                    and current_prefix <= self.LZ_MAX_CODE):
-                    stack[stack_ptr++] = suffix[current_prefix]
-                    current_prefix = self.prefix[current_prefix];
-                
-                if j >= self.LZ_MAX_CODE or current_prefix > self.LZ_MAX_CODE:
-                    raise Exception ("Image Defect")
+            plain_text =self.load_plain_text_extension()
+            if plain_text:
+                self.frames.append({'frame':frame,'type':'text','data':plain_text})
+                frame+=1
 
-                
-                stack[stack_ptr++] = current_prefix;
-
-                
-                while (stack_ptr != 0 self.and i < length)
-                    line[i++] = stack[--stack_ptr];
-                }
-                
-                if (prev_code != NO_SUCH_CODE) {
-                    if ((self.running_code < 2) ||
-                    (self.running_code > LZ_MAX_CODE+2))
-                    raise Exception ("Image Defect")
-                    prefix[self.running_code - 2] = prev_code;
-
-                    if (current_code == self.running_code - 2) {
-                        suffix[self.running_code - 2]
-                        = trace_prefix(prefix, prev_code, clear_code);
-                else:
-                    suffix[self.running_code - 2]= trace_prefix(prefix, current_code, clear_code);
-                prev_code = current_code;
-
-            self.prev_code = prev_code;
-            self.stack_ptr = stack_ptr;
-
-
-
-
+                continue
+            # stuff at the start of the file... not realy frame dependant?
+            application=self.load_application_extension()
+            if application:
+                self.applications.append(application)
+                continue
+            
+            # EOF
+            trailer=self.load_trailer()
+            if trailer:
+                print ("END POSITION: {0:02X}".format(self.stream.pos))
+                break
+            print ("POSITION: {0:02X}".format(self.stream.pos))
+            if old_pos==self.stream.pos:
+                raise Exception ("Forever loop. Death.")
+            old_pos=self.stream.pos
+                        #break
+        self.frames=frame
+        print("Stats")
+        print("  Frames: {0}".format(self.frames))
+        self.stream.close()
 
 
 
+    def load_image_descriptor(self):
+        try:
+            self.stream.pin()
+            descriptor=ImageDescriptor(self.stream)
+            descriptor.debug()
+            return descriptor
+        except Exception as ex:
+            #print("Trying:{0}".format(ex))
+            self.stream.rewind()
 
+    def load_comment_extension(self):
+        try:
+            self.stream.pin()
+            comment=CommentExtension(self.stream)
+            comment.debug()
+            return coment
+        except Exception as ex:
+            #print("Trying:{0}".format(ex))
+            self.stream.rewind()
+            
+    def load_graphics_control_extension(self):
+        try:
+            self.stream.pin()
+            graphiccontrol=GraphicsControlExtension(self.stream)
+            graphiccontrol.debug()
+            return graphiccontrol
+        except Exception as ex:
+            #print("Trying:{0}".format(ex))
+            self.stream.rewind()
 
+    def load_plain_text_extension(self):
+        try:
+            self.stream.pin()
+            plaintext=PlainTextExtension(self.stream)
+            plaintext.debug()
+            return plaintext
+        except Exception as ex:
+            #print("Trying:{0}".format(ex))
+            self.stream.rewind()
+    
+    def load_application_extension(self):
+        try:
+            self.stream.pin()
+            applicaitonextension=ApplicationExtension(self.stream)
+            applicaitonextension.debug()
+            return applicaitonextension
+        except Exception as ex:
+            #print("Trying:{0}".format(ex))
+            self.stream.rewind()
 
+    def load_trailer(self):
+        try:
+            self.stream.pin()
+            trailer=Trailer(self.stream)
+            trailer.debug()
+            return trailer
+        except Exception as ex:
+            #print("Trying:{0}".format(ex))
+            self.stream.rewind()
 
-def init_lookup_table(self):
-    colors=64
-    table_size=4096
-    eoi=1
-    clear=colors*2
-    lookup=[0]*(table_size+2)
-    for index in range(0,colors):
-        lookup[index]=index
-    lookup[table_size]=clear
-    lookup[table_size+1]=eoi
+    def load_color_table(self,entries):
+        try:
+            self.stream.pin()
+            colortable=ColorTable(self.stream,entries)
+            colortable.debug()
+            return colortable
+        except Exception as ex:
+            #print("Trying:{0}".format(ex))
+            self.stream.rewind()
 
-
-def read_code(self):
-        # data mask based based on the curent bit state
-        code_masks=[0x0000, 0x0001, 0x0003, 0x0007,0x000F, 0x001F, 0x003F, 0x007F,0x00FF, 0x01ff, 0x03FF, 0x07FF,0x0FFF]
-        
-        # if there isnt enough data in the byte buffer
-        # grab another one
-        # shift it by the shiift state
-        # "or" it with the curent byte
-        # now we have a buffer with enough data
-        while (self.shift_state < self.running_bits){
-            next_byte =stream.byte()
-            self.buffer =self.buffer  | next_byte << self.shift_state
-            self.shift_state += 8
-        }
-
-        # woo hoo. extract the code form the buffer
-        code = self.buffer & code_masks[self.running_bits]
-
-        # Shift the buffer by the number of bits extracted.
-        self.buffer  >>= self.running_bits
-        self.shift_state-= self.running_bits
-
-        self.decoded_code_index+=1
-        if self.decoded_code_index > self.max_code_plus_one and self.running_bits < self.LZ_BITS:
-            self.max_code_plus_one=self.max_code_plus_one << 1
-            self.running_bits+=1
-        return code
-
-
-def decode(self):
-    self.shifted=0
-    self.runnin=0
-    self.init_lookup_table()
-
-    code=0
-    old_code=0
-
-    loop
-        if code==clear:
-            self.init_lookup_table()
-        code=read_code()
-
-
-
-
-
-
-
-
-
-
-
-
+    def load_image_data(self):
+        #try:
+            self.stream.pin()
+            imagedata=ImageData(self.stream,self.header.NumberOfGlobalColorTableEntries)
+            imagedata.debug()
+            return imagedata
+        #except Exception as ex:
+         #   print("Trying:{0}".format(ex))
+         #   self.stream.rewind()
--- zlurp/gif/ImageData.py
+++ zlurp/gif/ImageData.py
@@ -2,8 +2,6 @@ class ImageData:
     def __init__(self,stream,colors):
         self.LZ_BITS         =12
         self.NOT_A_CODE      =4096
-        self.LZ_MAX_CODE     =4095    # Largest 12 bit code
-        self.MAX_TABLE_SIZE  =256    # Impossible code = flush
         self.colors=colors
         # where the data is stored as its pulled out of the stream
        
@@ -22,17 +20,17 @@ class ImageData:
         self.init_lookup_table()
         self.get_first_code_in_data_sub_block()           # always an init table
 
-        gif_index=self.read_block(block_size)
-
-        self.init_lookup_table()
-        block_size=self.stream.byte()
-        self.LWZ_MIN_BYTE_SIZE=8
-        print(block_size)
-        self.bytes_in_buffer=0
-        self.buffer=0
-
-        gif_index=self.read_block(block_size)
-        print(block_size)
+        #self.init_lookup_table()
+        
+        while block_size>0:
+            gif_index=self.read_block(block_size)
+            self.init_lookup_table()
+            block_size=self.stream.byte()
+            self.buffer=0
+            self.bytes_in_buffer=0
+            #self.LWZ_ByteSize=self.LWZ_MIN_BYTE_SIZE
+            print("Block Size: {0}".format(block_size))
+            print("Leftover bytes:{0}".format(self.bytes_in_buffer))
 
         #print ("Offset: {0:02X}, Size: {1:02X}".format(stream.pos,block_size))
         self.blocks.append({'size':block_size,'data':gif_index,'offset':self.DataLength})
@@ -45,42 +43,32 @@ class ImageData:
         print("  LWZ_ByteSize: {0:02X}".format(self.LWZ_ByteSize))
         print("Data: {0}".format(self.blocks))
 
+    def init_lookup_table(self):
+        
+        # reset these
+        self.LWZ_ByteSize=self.LWZ_MIN_BYTE_SIZE+1
+        self.CLEAR=1<<(self.LWZ_MIN_BYTE_SIZE)
+        self.END_OF_INFORMATION=self.CLEAR+1
+        self.set_max_code_size()
+       # print("LWZ_ByteSize: {0:02x}".format(self.LWZ_ByteSize))
+        print("CLEAR: {0:02x}".format(self.CLEAR))
+        print("END_OF_INFORMATION: {0:02x}".format(self.END_OF_INFORMATION))
+       # print("MAX_CODE_SIZE:{0:02x}".format(self.MAX_CODE_SIZE))
+        #self.lookup=[self.NOT_A_CODE]*(self.END_OF_INFORMATION+1)
+        self.lookup=[self.NOT_A_CODE]*(self.MAX_CODE_SIZE+2)
+        for index in range(0,self.MAX_CODE_SIZE):
+            self.lookup[index]=[index]
 
-    def read_block(self,block_size):
-        #Read first code
-        self.last_code=self.read_code()
-        gif_index=[self.last_code] 
-        while self.block_index < block_size:
-            
-            # pull another code from the compressed srream
-            #print(self.lookup)
-            code=self.read_code()
-            print("Index: 0x{0:02x}, Code: {1:02x}, Max code: {2:02x} Lookup Index {3:02X} BI:{4},BS:{5}".format(self.code_index,code,self.MAX_CODE_SIZE,self.next_table_index,self.block_index,block_size))
-            if self.block_index==block_size-1:
-                break                
-            if code==self.CLEAR:
-                print ("CLEAR")
-                self.init_lookup_table()
-                continue
-            elif code==self.END_OF_INFORMATION:
-                print ("EOI")
-                break
-             
-            # is the index in the lookup table?
-            if self.is_code_in_the_lookup(code):
-                gif_index+=self.lookup[code]
-                prefix=self.lookup[code][0]
-                new_code=self.lookup[self.last_code]+[prefix]
-            else:
-                print(self.lookup)
-                prefix=self.lookup[self.last_code][0]
-                new_code=self.lookup[self.last_code]+[prefix]
-                gif_index+=new_code
-            self.add_code_list_to_lookup(new_code)
-            self.last_code=code
-        # terminator=self.read_code()
-            #print ("Terminator: {0:02x}".format(terminator))
-        print ("Main loop")
+        self.lookup[self.CLEAR]=[self.CLEAR]
+        self.lookup[self.END_OF_INFORMATION]=[self.END_OF_INFORMATION]
+        self.next_table_index=self.END_OF_INFORMATION+1
+        self.block_index=0
+        self.code_index=0
+        
+    def set_max_code_size(self):
+        self.MAX_CODE_SIZE=(1<<(self.LWZ_ByteSize))-2
+        print ("MAX CODE SIZE: {0}".format(self.MAX_CODE_SIZE))
+   
     def get_first_code_in_data_sub_block(self):
         code=self.read_code()
         if code==self.CLEAR:
@@ -96,13 +84,9 @@ class ImageData:
             #print ("Bit size increase: {0}".format(self.LWZ_ByteSize))
             self.set_max_code_size()
             #print ("MAXCODE: {0:02X}".format(self.MAX_CODE_SIZE))
+            self.lookup=self.lookup+ [self.NOT_A_CODE]*(self.MAX_CODE_SIZE-len(self.lookup)+2)
 
-
-        if self.next_table_index>=len(self.lookup):
-            # print("Stretching lookup table")
-            # print (self.next_table_index-len(self.lookup)+1)
-            self.lookup=self.lookup+ [self.NOT_A_CODE]*(self.next_table_index-len(self.lookup)+1)
-
+        
         this_code_index=self.next_table_index
         #print("Table index",self.next_table_index)
         self.lookup[self.next_table_index]=code_list
@@ -118,31 +102,7 @@ class ImageData:
         if self.lookup[code]==self.NOT_A_CODE:
             return None
         return True
-
-    def init_lookup_table(self):
-        
-        # reset these
-        self.LWZ_ByteSize=self.LWZ_MIN_BYTE_SIZE+1
-        self.CLEAR=1<<(self.LWZ_MIN_BYTE_SIZE)
-        self.END_OF_INFORMATION=self.CLEAR+1
-        self.set_max_code_size()
-       # print("LWZ_ByteSize: {0:02x}".format(self.LWZ_ByteSize))
-        print("CLEAR: {0:02x}".format(self.CLEAR))
-        print("END_OF_INFORMATION: {0:02x}".format(self.END_OF_INFORMATION))
-       # print("MAX_CODE_SIZE:{0:02x}".format(self.MAX_CODE_SIZE))
-        #self.lookup=[self.NOT_A_CODE]*(self.END_OF_INFORMATION+1)
-        self.lookup=[self.NOT_A_CODE]*(4096)
-        for index in range(0,self.colors):
-            self.lookup[index]=[index]
-
-        self.lookup[self.CLEAR]=[self.CLEAR]
-        self.lookup[self.END_OF_INFORMATION]=[self.END_OF_INFORMATION]
-        self.next_table_index=self.END_OF_INFORMATION+1
-        self.block_index=0
-        self.code_index=0
-        
-        
-        
+  
     def read_code(self):
         # data mask based based on the curent bit state
         code_masks=[0x0000, 0x0001, 0x0003, 0x0007,0x000F, 0x001F, 0x003F, 0x007F,0x00FF, 0x01ff, 0x03FF, 0x07FF,0x0FFF]
@@ -179,6 +139,55 @@ class ImageData:
         self.code_index+=1
        
         return code
+ 
+    def read_block(self,block_size):
+        #Read first code
+        self.last_code=self.read_code()
+        gif_index=[self.last_code] 
+        while self.block_index < block_size:
+            
+            # pull another code from the compressed srream
+            #print(self.lookup)
+            code=self.read_code()
+            print("Index: 0x{0:02x}, Code: {1:02x}, Max code: {2:02x} Lookup Index {3:02X} BI:{4},BS:{5}".
+                    format(self.code_index,code,self.MAX_CODE_SIZE,self.next_table_index,self.block_index,block_size))
+            #if self.block_index==block_size-1:
+            #    break                
+            if code==self.CLEAR:
+                print ("Processing CLEAR")
+                self.init_lookup_table()
+                continue
 
-    def set_max_code_size(self):
-        self.MAX_CODE_SIZE=(1<<(self.LWZ_ByteSize))-2
+            elif code==self.END_OF_INFORMATION:
+                print ("Processing EOI")
+                break
+             
+            
+            if self.last_code>=len(self.lookup):
+                raise Exception ("Last Code out of bounds: lookup length: {0}. Code: {1}".format(len(self.lookup),self.last_code))
+            
+            
+            if self.lookup[self.last_code]==self.NOT_A_CODE:
+                raise Exception ("Last Code has no value: lookup length: {0}. Code: {1} , Next Code Index: {2}".format(len(self.lookup),self.last_code,self.next_table_index))
+            # is the index in the lookup table?
+            if self.is_code_in_the_lookup(code):
+                #if code>=len(self.lookup):
+                #    raise Exception ("Code out of bounds: lookup length: {0}. Code: {1}".format(len(self.lookup),code))
+            
+                #if self.lookup[code]==self.NOT_A_CODE:
+                #    raise Exception ("Code has no value: lookup length: {0}. Code: {1}, Next Code Index: {2}".format(len(self.lookup),code,self.next_table_index))
+
+                gif_index+=self.lookup[code]
+                prefix=self.lookup[code][0]
+                new_code=self.lookup[self.last_code]+[prefix]
+            else:
+                print(self.lookup)
+                prefix=self.lookup[self.last_code][0]
+                new_code=self.lookup[self.last_code]+[prefix]
+                gif_index+=new_code
+            self.add_code_list_to_lookup(new_code)
+            self.last_code=code
+        # terminator=self.read_code()
+            #print ("Terminator: {0:02x}".format(terminator))
+        print ("End of read Block")
+    
