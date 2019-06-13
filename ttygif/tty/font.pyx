6d4a446a39adfdafdc5ce151dca9efe9f55d7f60
Charles Watkins <charles@titandws.com>
2019-06-13 14:48:38 -0400

    Bump Version 1.0.66

--- ttygif/tty/font.pyx
+++ ttygif/tty/font.pyx
@@ -3,7 +3,7 @@ import array
 
 cdef class font_map:
 
-    cdef char *name
+    name
     cdef int        chars_per_line
     cdef int        lines         
     cdef int        width         
--- ttygif/tty/viewer.pyx
+++ ttygif/tty/viewer.pyx
@@ -1,18 +1,14 @@
 from cpython cimport array
 import array
+import re
 
 from .fonts import font
-import re
+
 # Reference
 # http://man7.org/linux/man-pages/man4/console_codes.4.html
 
 
 cdef class viewer:
-    # TODO Fix self... on font
-    # create loader from gif...
-    # save byt3stream into file manually
-    # load bytestream
-    # automate this with a funciton
     cdef int   viewport_px_width
     cdef int   viewport_px_height
     cdef int   viewport_char_height
