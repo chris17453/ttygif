b0eda80b8f2d4603431d7ef2692aec5e18969a6f
Charles Watkins <charles@titandws.com>
2019-06-13 18:18:23 -0400

    Bump Version 1.0.151

--- ttygif/tty/viewer.pyx
+++ ttygif/tty/viewer.pyx
@@ -11,13 +11,13 @@ from .fonts cimport font
 
 
 class viewer:
-    cdef public int         debug
-    cdef public int         viewport_px_width
-    cdef public int         viewport_px_height
-    cdef public int         viewport_char_height
-    cdef public int         viewport_char_width
-    cdef public int         background_color
-    cdef public int         foreground_color
+    cint         debug
+    cint         viewport_px_width
+    cint         viewport_px_height
+    cint         viewport_char_height
+    cint         viewport_char_width
+    cint         background_color
+    cint         foreground_color
     cdef public char*       window_style
     cdef public char*       stream
     cdef public int         video_length
