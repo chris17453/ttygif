f0379d28686d66c75130e848df02cad58f631152
Charles Watkins <charles@titandws.com>
2019-06-13 16:52:45 -0400

    Bump Version 1.0.108

--- ttygif/tty/viewer.pxd
+++ ttygif/tty/viewer.pxd
@@ -1,2 +1,9 @@
 cdef class viewer:
-    cdef int debug
+    cdef int   debug
+    cdef int   viewport_px_width
+    cdef int   viewport_px_height
+    cdef int   viewport_char_height
+    cdef int   viewport_char_width
+    cdef int   background_color
+    cdef int   foreground_color
+    cdef char* window
--- ttygif/tty/viewer.pyx
+++ ttygif/tty/viewer.pyx
@@ -11,14 +11,7 @@ from .fonts cimport font
 
 
 cdef class viewer:
-    cdef int   debug
-    cdef int   viewport_px_width
-    cdef int   viewport_px_height
-    cdef int   viewport_char_height
-    cdef int   viewport_char_width
-    cdef int   background_color
-    cdef int   foreground_color
-    cdef char* window
+  
 
     def info(self,text):
         return
