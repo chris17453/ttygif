62c0175df2f3c96d5b0c27e140ae8342fb8dc6b7
Charles Watkins <charles@titandws.com>
2019-06-13 14:27:56 -0400

    Bump Version 1.0.50

--- ttygif/tty/font.pyx
+++ ttygif/tty/font.pyx
@@ -3,17 +3,17 @@ import array
 
 cdef class font:
 
-    char *      name          =""
-    cint        chars_per_line=0
-    cint        lines         =0
-    cint        width         =0
-    cint        height        =0
-    cint        font_width    =0
-    cint        font_height   =0
-    cint        spacing_x     =0
-    cint        spacing_y     =0
-    cint        offset_x      =0
-    cint        offset_y      =0
-    array.array color_table
-    cint        transparent   =0
-    array.array graphics
+    cdef char *      name          =""
+    cdef int        chars_per_line=0
+    cdef int        lines         =0
+    cdef int        width         =0
+    cdef int        height        =0
+    cdef int        font_width    =0
+    cdef int        font_height   =0
+    cdef int        spacing_x     =0
+    cdef int        spacing_y     =0
+    cdef int        offset_x      =0
+    cdef int        offset_y      =0
+    cdef array.array color_table
+    cdef int        transparent   =0
+    cdef array.array graphics
