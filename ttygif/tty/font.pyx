6b38b75762296e7f59e92e9e8aa469892db07a98
Charles Watkins <charles@titandws.com>
2019-06-13 14:23:50 -0400

    Bump Version 1.0.45

--- ttygif/tty/font.pyx
+++ ttygif/tty/font.pyx
@@ -3,7 +3,7 @@ import array
 
 class font:
 
-    cdef  char*      name          =""
+    name          =""
     cdef cint        chars_per_line=0
     cdef cint        lines         =0
     cdef cint        width         =0
