f449fd48da67d3ee636e66f3506f594493cabd70
Charles Watkins <charles@titandws.com>
2019-06-15 18:02:51 -0400

    Bump Version 1.0.277

--- ttygif/cast2gif.pyx
+++ ttygif/cast2gif.pyx
@@ -88,7 +88,7 @@ class cast2gif:
             old_percent=percent
             percent=int((index*100)/strlen)
             if percent!=old_percent:
-                sys.stdout.write("Seconds: {0} of {1} {2}% {3} FPS or {4}ms    \r\n".format(timestamp,last_timestamp,percent,frame_rate,interval))
+                sys.stdout.write("Seconds: {0} of {1} {2}% {3} FPS ({4}ms)    \r".format(timestamp,last_timestamp,percent,frame_rate,interval))
                 sys.stdout.flush()
             cur_timestamp=float(event[0])
             #print cur_timestamp,cur_timestamp-timestamp,interval
