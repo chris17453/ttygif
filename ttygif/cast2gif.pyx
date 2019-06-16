4bd899e8f5e5377dc8fb050a73ae496d8f7c0030
Charles Watkins <charles@titandws.com>
2019-06-16 19:28:59 -0400

    Bump Version 1.0.423

--- ttygif/cast2gif.pyx
+++ ttygif/cast2gif.pyx
@@ -115,7 +115,7 @@ class cast2gif:
                 v.render()
                 old_data=data
                 data=v.get()
-                old_data=None
+                #old_data=None
                 diff=self.get_frame_bounding_diff(old_data,data,v.viewport_px_width,v.viewport_px_height)
                 
                 if diff:
