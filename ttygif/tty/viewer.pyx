ac8d7e657fd09cb8c7a41c63ecfaf73cce694c47
Charles Watkins <charles@titandws.com>
2019-06-16 17:02:04 -0400

    Bump Version 1.0.364

--- ttygif/tty/viewer.pyx
+++ ttygif/tty/viewer.pyx
@@ -715,7 +715,7 @@ cdef class viewer:
             c=text[i]
             r=self.remap_character(c)
             remapped[i]=r
-        text="".join(remapped)
+        text=u"".join(remapped)
         
         self.info ("Text: '{0}' Length:{1} Timestamp:{2}".format(self.ascii_safe(text),len(text),timestamp))
         self.sequence.append({'type':'text','data':text,'timestamp':timestamp})
