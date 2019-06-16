0b3583bb4fc19da0f6c6688d7fc1ef6544439bfb
Charles Watkins <charles@titandws.com>
2019-06-16 16:06:34 -0400

    Bump Version 1.0.353

--- ttygif/tty/viewer.pyx
+++ ttygif/tty/viewer.pyx
@@ -705,7 +705,7 @@ cdef class viewer:
     def add_text_sequence(self,text,timestamp):
         if len(text)==0:
             return
-        text=[self.remap_character(i) for i in text]
+        text=''.join([self.remap_character(i) for i in text])
         self.info ("Text: '{0}' Length:{1} Timestamp:{2}".format(self.ascii_safe(text),len(text),timestamp))
         self.sequence.append({'type':'text','data':text,'timestamp':timestamp})
 
