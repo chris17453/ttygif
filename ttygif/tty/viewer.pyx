15ae6b33f9a748fc52ba430da759fed6957a3fc4
Charles Watkins <charles@titandws.com>
2019-06-16 14:22:14 -0400

    Bump Version 1.0.324

--- ttygif/tty/viewer.pyx
+++ ttygif/tty/viewer.pyx
@@ -17,6 +17,7 @@ cdef class viewer:
     cdef public int         viewport_px_height
     cdef public int         viewport_char_height
     cdef public int         viewport_char_width
+    cdef public int         viewport_char_stride
     cdef public int         background_color
     cdef public int         foreground_color
     cdef public object      window_style
