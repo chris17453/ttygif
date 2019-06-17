8c81786e2fa9a24e60a703631bd4dc2dda6addbb
Charles Watkins <charles@titandws.com>
2019-06-16 20:02:04 -0400

    Bump Version 1.0.434

--- ttygif/cast2gif.pyx
+++ ttygif/cast2gif.pyx
@@ -1,5 +1,6 @@
 # cython: linetrace=True
 import sys
+from libc.string cimport  memcpp
 from .gif.encode import encode_gif
 from .asciicast.reader import asciicast_reader
 from .tty.viewer import viewer
