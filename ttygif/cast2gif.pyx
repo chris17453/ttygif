e39cfeb623527ddf2f9c4c210ede561c9835141f
Charles Watkins <charles@titandws.com>
2019-06-19 22:44:49 -0400

    Bump Version 1.0.623

--- ttygif/cast2gif.pyx
+++ ttygif/cast2gif.pyx
@@ -208,7 +208,7 @@ cdef class cast2gif:
         if self.debug:
             v.debug_sequence()
         g.close()
-        #print text
+        print ("Total:".self.aggregate_timestamp)
         print("\nfinished")
         
 
