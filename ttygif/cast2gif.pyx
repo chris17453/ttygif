0d8b6e1d7b0939e07818852df5ec38c5db0ab58b
Charles Watkins <charles@titandws.com>
2019-06-19 22:41:29 -0400

    Bump Version 1.0.622

--- ttygif/cast2gif.pyx
+++ ttygif/cast2gif.pyx
@@ -138,7 +138,7 @@ cdef class cast2gif:
         if self.frame_rate!=0:
             self.interval=float(1)/float(self.frame_rate)
         else:
-            self.interval=.05
+            self.interval=.10
         frame=0
         data=None
         old_data=None
