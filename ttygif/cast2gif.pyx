54cc865d2e370959b56fa2d2538ed9876e28fbc5
Charles Watkins <charles@titandws.com>
2019-06-23 18:18:35 -0400

    Bump Version 1.0.755

--- ttygif/cast2gif.pyx
+++ ttygif/cast2gif.pyx
@@ -149,12 +149,13 @@ cdef class cast2gif:
     
     def encode_stream(self):
         g=encode_gif(self.loop_count,debug=self.debug)
-        print ("character dimensions: {0}x{1}".format(self.width,self.height))
-        print ("pixel dimensions: {0}x{1}".format(v.viewport_px_width,v.viewport_px_height))
         
 
         v=viewer(char_width=self.width,char_height=self.height,debug=self.debug)
         g.create(width=v.viewport_px_width,height=v.viewport_px_height,filename=self.gif_file,default_palette=True)
+
+        print ("character dimensions: {0}x{1}".format(self.width,self.height))
+        print ("pixel dimensions: {0}x{1}".format(v.viewport_px_width,v.viewport_px_height))
 
         index=0
         if self.frame_rate!=0:
