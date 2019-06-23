2bd0ede8708b95007fd8a40bd25c0dd54bb3c58f
Charles Watkins <charles@titandws.com>
2019-06-23 19:32:13 -0400

    Bump Version 1.0.782

--- ttygif/tools/passthrough.py
+++ ttygif/tools/passthrough.py
@@ -16,10 +16,11 @@ def read_stdin():
     events=[]
     try:
         while has_stdin():
-            o= sys.stdin.readline()
-            print '1'
-            if ""==o: # empty line will be \n
+            if sys.stdin.closed:    
                 break
+            o= sys.stdin.readline()
+            if ""==o: # empty line will be \n
+                continue
             timestamp=round(time.time(),8)-ts
             #print timestamp
             events.append([timestamp,'o',o])
