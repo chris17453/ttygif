496228f40ec6a21732794878841dc97ae8aec7a9
Charles Watkins <charles@titandws.com>
2019-06-23 18:41:53 -0400

    Bump Version 1.0.758

--- ttygif/cli.py
+++ ttygif/cli.py
@@ -27,6 +27,8 @@ def cli_main():
     parser.add_argument('--output',    help='gif output file', default= None,metavar='FILE')
     parser.add_argument('--loop',      help='number of loops to play, 0=unlimited', default=0,metavar='COUNT')
     parser.add_argument('--delay',     help='delay before restarting gif in milliseconds ', default=100,metavar='MS')
+    parser.add_argument('--record',    help='output generated cast data to file', metavar='FILE')
+    
     parser.add_argument('--dilation',  help='process events at a faster or slower rate of time', default=1,metavar='RATE', type=float)
     parser.add_argument('--fps',       help='encode at (n) frames per second (0-25) 0=speed of cast file, min 3ms', default=0,metavar='FPS', type=int)
     parser.add_argument('--width',     help='change character width of gif, default is 80 or what is in the cast file',metavar='WIDTH', type=int)
@@ -74,6 +76,9 @@ def cli_main():
         events=None
         if has_stdin():
             events=read_stdin()
+            ar=asciicast_reader()
+            ar.write(events,file)
+            
         elif None==args.input:
             print "1"
             parser.print_help()    
