6fe81bcdadc2b2dc9d3c5d53f98a117b2a449371
Charles Watkins <charles@titandws.com>
2019-06-23 17:26:35 -0400

    Bump Version 1.0.742

--- ttygif/cast2gif.pyx
+++ ttygif/cast2gif.pyx
@@ -84,7 +84,7 @@ cdef class cast2gif:
         else:
             if self.frame_rate==0:
                 #print self.stream['events'][event_index+1][0],self.stream['events'][event_index][0]
-                delay=int((self.stream['events'][event_index+1][0]-self.stream['events'][event_index][0])*self.dilation*10)
+                delay=int((self.stream['events'][event_index+1][0]-self.stream['events'][event_index][0])*100)
             else:
                 delay=0
         return delay
