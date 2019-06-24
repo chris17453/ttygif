0bf05e7121e14e7a098753189d963db4a16015b4
Charles Watkins <charles@titandws.com>
2019-06-24 19:53:59 -0400

    Bump Version 1.0.812

--- ttygif/gif/stream.pyx
+++ ttygif/gif/stream.pyx
@@ -4,6 +4,26 @@ import os
 import struct 
 
 cdef class DataStream:
+    cdef object FILE_NULL
+    cdef object FILE_NOT_FOUND
+    cdef object FILE_OBJECT_NULL
+    cdef object OUT_OF_BOUNDS
+    cdef object INVALID_POSITION
+    cdef object mode
+    cdef public long   pos
+    cdef long   file_length
+    cdef object file
+    cdef object file_object
+    cdef long  pinned_position
+
+    cdef validate_file(self)
+    cdef validate_bounds(self)
+    cdef open(self)
+    cdef pin(self)
+    cdef seek(self,position)
+    cdef rewind(self)
+    cdef get_file_size(self)
+    
 
 
     def __cinit__(self,file=None,mode="r"):
