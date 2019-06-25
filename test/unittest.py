9a40dda31786a631c9e8782e2d7895ffb8c6090e
Charles Watkins <charles@titandws.com>
2019-06-24 20:50:22 -0400

    Bump Version 1.0.841

--- test/unittest.py
+++ test/unittest.py
@@ -11,35 +11,35 @@ class test_engine(unittest2.TestCase):
     def test_canvas_noninterlaced(self):
         try:
             g=ttygif.gif.gif(debug=None)
-            g.canvas_it(base_dir+"/VGA_8x19font.gif",base_decode_dir+"/VGA_8x19font.html")
+            g.canvas_it(base_asset_dir+"/VGA_8x19font.gif",base_decode_dir+"/VGA_8x19font.html")
         except Exception as ex:
             self.fail(ex)
 
     def test_canvas_interlaced(self):
         try:
             g=ttygif.gif(debug=None)
-            g.canvas_it(base_dir+"/89a_interlaced.gif",base_decode_dir+"/89a_interlaced.html")
+            g.canvas_it(base_asset_dir+"/89a_interlaced.gif",base_decode_dir+"/89a_interlaced.html")
         except Exception as ex:
             self.fail(ex)
 
     def test_canvas_kermit(self):
         try:
             g=ttygif.gif(debug=None)
-            g.canvas_it(base_dir+"/kermit.gif",base_decode_dir+"/kermit.html")
+            g.canvas_it(base_asset_dir+"/kermit.gif",base_decode_dir+"/kermit.html")
         except Exception as ex:
             self.fail(ex)
 
     def test_canvas_giphy(self):
         try:
             g=ttygif.gif(debug=None)
-            g.canvas_it(base_dir+"/giphy.gif",base_decode_dir+"/giphy.html")
+            g.canvas_it(base_asset_dir+"/giphy.gif",base_decode_dir+"/giphy.html")
         except Exception as ex:
             self.fail(ex)
 
     def test_canvas_blue(self):
         try:
             g=ttygif.gif(debug=None)
-            g.canvas_it(base_dir+"/blue.gif",base_decode_dir+"/blue.html")
+            g.canvas_it(base_asset_dir+"/blue.gif",base_decode_dir+"/blue.html")
         except Exception as ex:
             self.fail(ex)
 
