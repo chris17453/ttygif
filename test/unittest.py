d3ef6a49a8a3c2ffc8a66cb6fdaa873b675b88b2
Charles Watkins <charles@titandws.com>
2019-06-24 20:49:58 -0400

    Bump Version 1.0.840

--- test/unittest.py
+++ test/unittest.py
@@ -6,41 +6,42 @@ from .context import  ttygif
 # some gifs came from https://www.htmlgoodies.com/tutorials/web_graphics/article.php/3479931/Image-Formats.htm#gif
 # the rest are random. Kermit is my fav
 class test_engine(unittest2.TestCase):
-
+    base_asset_dir="assets/examples/src_gifs/"
+    base_decode_dir="assets/examples/src_gifs/"
     def test_canvas_noninterlaced(self):
-        #try:
+        try:
             g=ttygif.gif.gif(debug=None)
-            g.canvas_it("assets/examples/src_gifs/VGA_8x19font.gif","assets/examples/decode/VGA_8x19font.html")
-        #except Exception as ex:
-        #    self.fail(ex)
+            g.canvas_it(base_dir+"/VGA_8x19font.gif",base_decode_dir+"/VGA_8x19font.html")
+        except Exception as ex:
+            self.fail(ex)
 
-  #  def test_canvas_interlaced(self):
-  #      try:
-  #          g=ttygif.gif(debug=None)
-  #          g.canvas_it("data/89a_interlaced.gif","examples/decode/89a_interlaced.html")
-  #      except Exception as ex:
-  #          self.fail(ex)
-#
-  #  def test_canvas_kermit(self):
-  #      try:
-  #          g=ttygif.gif(debug=None)
-  #          g.canvas_it("data/kermit.gif","examples/decode/kermit.html")
-  #      except Exception as ex:
-  #          self.fail(ex)
-#
-  #  def test_canvas_giphy(self):
-  #      try:
-  #          g=ttygif.gif(debug=None)
-  #          g.canvas_it("data/giphy.gif","examples/decode/giphy.html")
-  #      except Exception as ex:
-  #          self.fail(ex)
-#
-  #  def test_canvas_blue(self):
-  #      try:
-  #          g=ttygif.gif(debug=None)
-  #          g.canvas_it("data/blue.gif","examples/decode/blue.html")
-  #      except Exception as ex:
-  #          self.fail(ex)
+    def test_canvas_interlaced(self):
+        try:
+            g=ttygif.gif(debug=None)
+            g.canvas_it(base_dir+"/89a_interlaced.gif",base_decode_dir+"/89a_interlaced.html")
+        except Exception as ex:
+            self.fail(ex)
+
+    def test_canvas_kermit(self):
+        try:
+            g=ttygif.gif(debug=None)
+            g.canvas_it(base_dir+"/kermit.gif",base_decode_dir+"/kermit.html")
+        except Exception as ex:
+            self.fail(ex)
+
+    def test_canvas_giphy(self):
+        try:
+            g=ttygif.gif(debug=None)
+            g.canvas_it(base_dir+"/giphy.gif",base_decode_dir+"/giphy.html")
+        except Exception as ex:
+            self.fail(ex)
+
+    def test_canvas_blue(self):
+        try:
+            g=ttygif.gif(debug=None)
+            g.canvas_it(base_dir+"/blue.gif",base_decode_dir+"/blue.html")
+        except Exception as ex:
+            self.fail(ex)
 
 
 if __name__ == '__main__':
