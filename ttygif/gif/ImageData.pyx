5861ca1e7a3c6f00c1ef79ab62dc7fa621c70c3a
Charles Watkins <charles@titandws.com>
2019-06-30 14:21:40 -0400

    Bump Version 1.1.269

--- ttygif/gif/ImageData.pyx
+++ ttygif/gif/ImageData.pyx
@@ -421,36 +421,6 @@ cdef class lzw_encode:
         self.write_bit(0)
       if self.chunk_pos>0:
         self.write_chunk()
-      
-
-
-
-def compress(uncompressed):
-    """Compress a string to a list of output symbols."""
- 
-    # Build the dictionary.
-    dict_size = 256
-    dictionary = dict((chr(i), i) for i in xrange(dict_size))
-    # in Python 3: dictionary = {chr(i): i for i in range(dict_size)}
- 
-    w = ""
-    result = []
-    for c in uncompressed:
-        wc = w + c
-        if wc in dictionary:
-            w = wc
-        else:
-            result.append(dictionary[w])
-            # Add wc to the dictionary.
-            dictionary[wc] = dict_size
-            dict_size += 1
-            w = c
- 
-    # Output the code for w.
-    if w:
-        result.append(dictionary[w])
-    return result
- 
 
 
 
