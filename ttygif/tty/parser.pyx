d1672a5f29124338315df757774d1f3ccbe9aa3c
Charles Watkins <charles@titandws.com>
2019-07-01 20:23:03 -0400

    Bump Version 1.1.381

--- ttygif/tty/parser.pyx
+++ ttygif/tty/parser.pyx
@@ -270,7 +270,7 @@ cdef class term_parser:
         cdef int CR=13     # x Carriage return
         #cdef int sx=self.g.state.cursor_x
         #cdef int sy=self.g.state.cursor_y
-        self.g.state.text_mode_off()
+        self.g.state.text_mode_on()
         for character in event['data']:
             char_ord=ord(character)
             if char_ord<32:
@@ -283,6 +283,7 @@ cdef class term_parser:
                     self.g.state.cursor_down(1)
             else:
                 self.g.write(char_ord)
+                 
                 self.g.state.cursor_right(1)
             
             while self.g.state.scroll!=0:
