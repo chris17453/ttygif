3584cac1e4aec79ce56211a1843a10ff8a8aeeb4
Charles Watkins <charles@titandws.com>
2019-07-04 22:23:18 -0400

    Bump Version 1.1.445

--- ttygif/tty/terminal_emulator.pyx
+++ ttygif/tty/terminal_emulator.pyx
@@ -21,7 +21,8 @@ cdef class terminal_emulator:
     cdef init(self,width,height,char_width,char_height,debug):
         #cdef font internal_font=font("Bm437_PhoenixEGA_9x14")
         #cdef font internal_font=font("Bm437_ATI_9x16")
-        cdef font internal_font=font("Bm437_Wyse700a")
+        #cdef font internal_font=font("Bm437_Wyse700a")
+        cdef font internal_font=font("Bm437_Wyse700a-2y")
         
         self.terminal_graphics= terminal_graphics(character_width = char_width,
                                                  character_height = char_height,
