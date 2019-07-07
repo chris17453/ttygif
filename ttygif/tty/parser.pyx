7c378cb139741878ac1bc44b8ba39645dcbe18d4
Charles Watkins <charles@titandws.com>
2019-07-07 17:54:15 -0400

    Bump Version 1.1.649

--- ttygif/tty/parser.pyx
+++ ttygif/tty/parser.pyx
@@ -648,6 +648,7 @@ cdef class term_parser:
         if event['type']=='text':
             txt="{2:6x} {3:3.5f} : text('{0},{1}')".format( self.ascii_safe(event['data']),len(event['data']),index, event['timestamp'] ) 
             print(txt)
+            return
         for cmd in commands:
             if cmd[1]==event['command'] and event['esc_type']==cmd[0]:
                 param=[]
