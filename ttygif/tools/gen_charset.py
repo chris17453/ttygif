1b165db2d258f85ea29a058fdf1f7761501a09eb
Charles Watkins <charles@titandws.com>
2019-07-07 01:49:45 -0400

    readme

--- ttygif/tools/gen_charset.py
+++ ttygif/tools/gen_charset.py
@@ -11,16 +11,16 @@ start_nocode=u"\033[200~"
 end_nocode=u"\033[201~"
 #print bracketed_paste_start.encode('latin-1'),
 
-print(bracketed_paste_start+"Font: {0}\r".format(name))
+print(bracketed_paste_start+" Font: {0}\r".format(name))
 
 
-o=["{0:03} ".format(0)]
+o=[" {0:03} ".format(0)]
 for i in range(0,256):
     o.append(unichr(i))
     if len(o)==64+1 or i==255:
         o.append(" {0:03}".format(i))
         print start_nocode.encode('latin-1')+ "".join(o).encode('latin-1') + end_nocode.encode('latin-1'),
         print u"\n\r", 
-        o=["{0:03} ".format(i+1)]
+        o=[" {0:03} ".format(i+1)]
 
 print bracketed_paste_end +"\r",
