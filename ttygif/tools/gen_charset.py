1cad5ff98926db391bc23c3029a69f0d2ac194ba
Charles Watkins <charles@titandws.com>
2019-07-07 00:57:23 -0400

    Bump Version 1.1.541

--- ttygif/tools/gen_charset.py
+++ ttygif/tools/gen_charset.py
@@ -14,14 +14,14 @@ for i in range(0,256):
     
     o.append(unichr(i))
     
-    if len(o)==33 or i==255:
-            o.append(" {0:03}".format(i))
-            print start_nocode , 
-            print "".join(o).encode('latin-1'),
-            print end_nocode ,
-            print u"\r\n" , 
-            o=["{0:03} ".format(i+1)]
-    
+    if len(o)==10 or i==255:
+        o.append(" {0:03}".format(i))
+        print start_nocode , 
+        print "".join(o).encode('latin-1'),
+        print end_nocode ,
+        print u"\r\n" , 
+        o=["{0:03} ".format(i+1)]
+
 
 print(bracketed_paste_end)
 
