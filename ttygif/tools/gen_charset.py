5392c930d9b06f9ef3854dca7d59dc034d9bbc0d
Charles Watkins <charles@titandws.com>
2019-07-07 01:42:22 -0400

    Bump Version 1.1.550

--- ttygif/tools/gen_charset.py
+++ ttygif/tools/gen_charset.py
@@ -1,4 +1,5 @@
 
+
 # -*- coding: utf-8 -*-
 import sys
 
@@ -8,18 +9,18 @@ bracketed_paste_end="\033[?2004l"
 bracketed_paste_start="\033[?2004h"
 start_nocode=u"\033[200~"
 end_nocode=u"\033[201~"
-print("Font: {0}\r".format(name))
-print bracketed_paste_start ,
+#print bracketed_paste_start.encode('latin-1'),
+
+print(bracketed_paste_start+"Font: {0}\r".format(name))
+
 
 o=["{0:03} ".format(0)]
 for i in range(0,256):
     o.append(unichr(i))
     if len(o)==64+1 or i==255:
         o.append(" {0:03}".format(i))
-        print start_nocode+"" ,
-        print "".join(o).encode('latin-1'),
-        print end_nocode ,
-        print u"\r\n" , 
+        print start_nocode.encode('latin-1')+ "".join(o).encode('latin-1') + end_nocode.encode('latin-1'),
+        print u"\n\r", 
         o=["{0:03} ".format(i+1)]
 
 print bracketed_paste_end +"\r",
