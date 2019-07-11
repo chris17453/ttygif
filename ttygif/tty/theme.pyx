cc7a50e4030b660031acf355620cc3b6c9990530
Charles Watkins <charles@titandws.com>
2019-07-10 21:48:07 -0400

    Bump Version 1.1.758

--- ttygif/tty/theme.pyx
+++ ttygif/tty/theme.pyx
@@ -34,7 +34,7 @@ cdef class layer:
         for frame in gif_raw['frames']:
             if frame['image']:
                 atrribs=frame['descriptor']
-                self.image=image(1,atrribs.width,atrribs.height,gif_raw['global_color_table'],0)
+                self.image=image(1,atrribs.Width,atrribs.Height,gif_raw['global_color_table'],0)
                 self.image.data=frame['image']
 
         
