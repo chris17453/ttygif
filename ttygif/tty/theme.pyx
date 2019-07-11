56828623cf1649ebf9af8f2ca644155020632c03
Charles Watkins <charles@titandws.com>
2019-07-10 20:00:47 -0400

    Bump Version 1.1.691

--- ttygif/tty/theme.pyx
+++ ttygif/tty/theme.pyx
@@ -47,6 +47,14 @@ class factory_json:
                 partial.append(self.render(item,depth=depth+1))
             if len(partial)>0:
                 fragment+=array_template.format(",".join(map(str, partial)))
+        
+        elif isinstance(obj,dict):
+            partial=[]
+            for item in obj:
+                partial.append(tuple_template.format(item,self.render(obj[item],depth=depth+1)))
+            if len(partial)>0:
+                fragment+=object_template.format(",".join(map(str, partial))) 
+
         elif isinstance(obj, (type, types.ClassType)):
             partial=[]
             items=self.props(obj)
