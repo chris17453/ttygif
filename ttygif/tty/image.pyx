3778a45128f506f4e348c501610273a2edee8762
Charles Watkins <charles@titandws.com>
2019-07-14 14:14:45 -0400

    Bump Version 1.1.944

--- ttygif/tty/image.pyx
+++ ttygif/tty/image.pyx
@@ -209,7 +209,8 @@ cdef class image:
             dst.left+=dst_image.dimentions.width-1
         if dst.top<0:
             dst.top+=dst_image.dimentions.height-1
-
+        src.debug()
+        dst.debug()
         for y in xrange(0,src.height):
             for x in xrange(0,src.width):
                 pixel=self.get_pixel(x+src.left,y+src.top)
