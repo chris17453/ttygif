3a2ad4139fdf7deace957d61c82cbae858324dc7
Charles Watkins <charles@titandws.com>
2019-07-14 15:26:59 -0400

    Bump Version 1.1.980

--- ttygif/tty/image.pyx
+++ ttygif/tty/image.pyx
@@ -129,7 +129,7 @@ cdef class image:
             err="Get Pixel Out of Bounds {0} of {1}".format(pos,self.dimentions.length)
             raise Exception (err)
         
-        element[0]=self.data[pos]
+        element=self.data[pos]
     
     cdef void get_pixel_3byte(self,int x,int y,uint8_t[3] element):
         cdef uint8_t pos=x*self.dimentions.bytes_per_pixel+y*self.dimentions.stride
