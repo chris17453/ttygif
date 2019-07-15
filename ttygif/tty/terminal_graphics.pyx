e0a070f3c6db03218ec629665ca3e466a857dd72
Charles Watkins <charles@titandws.com>
2019-07-14 20:01:17 -0400

    Bump Version 1.2.32

--- ttygif/tty/terminal_graphics.pyx
+++ ttygif/tty/terminal_graphics.pyx
@@ -187,7 +187,7 @@ cdef class terminal_graphics:
         if  temp.mode=="3slice":
             temp.image.copy_3slice(self.viewport,temp.outer,temp.inner,temp.dst,temp.copy_mode)
         elif temp.mode=="copy":
-            temp.image.copy(self.viewport,temp.bounds,temp.dst.point1() )
+            temp.image.copy(self.viewport,temp.bounds,temp.dst )
 
 
     cdef render(self):
