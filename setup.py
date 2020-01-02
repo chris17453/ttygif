635986814bd75ae8cf07848e1a948da17b1bae9a
Charles Watkins <chris17453@gmail.com>
2020-01-01 19:30:05 -0500

    module update

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 # cython: language_level=2
 import os
 import sys
-from setuptools import setup, find_packages
+from setuptools import setup, fin
+d_packages
 from setuptools.extension import Extension
 from Cython.Build import cythonize
 
