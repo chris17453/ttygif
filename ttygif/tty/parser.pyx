2404c4e5f3f3f03f49ed96fdee75fba1e1435894
chris17453 <chris17453@gmail.com>
2021-08-27 14:27:58 -0400

    Bump Version 1.2.273

--- ttygif/tty/parser.pyx
+++ ttygif/tty/parser.pyx
@@ -742,10 +742,10 @@ cdef class term_parser:
                                             event['timestamp'],
                                             self.g.state.cursor_x,
                                             self.g.state.cursor_y))
-                return
+             else:    
                 
 
-        print("{5:05d} {6:03d}x{7:03d} CMD:  '{0}', Name:'{3}', Command:{1}, Params:{2}  Timestamp:{4}".format(event['esc_type'],
+                print("{5:05d} {6:03d}x{7:03d} CMD:  '{0}', Name:'{3}', Command:{1}, Params:{2}  Timestamp:{4}".format(event['esc_type'],
                                             event['command'],
                                             event['params'],
                                             event['name'],
