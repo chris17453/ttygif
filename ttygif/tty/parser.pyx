68a71cd80de2f0b1477f35d8b052a31ff72bc391
chris17453 <chris17453@gmail.com>
2021-08-27 20:31:13 -0400

    Bump Version 1.2.350

--- ttygif/tty/parser.pyx
+++ ttygif/tty/parser.pyx
@@ -402,6 +402,7 @@ cdef class term_parser:
     
     
     cdef cmd_render_text(self,event):
+        print(" - RENDER TO BUFFER")
         #print event['data']
        
         cdef int BS=8     # x Backspace
@@ -432,7 +433,7 @@ cdef class term_parser:
             while self.g.state.scroll!=0:
                 #print("Scroll at {0:005x}".format(self.current_sequence_position))
                 self.g.scroll_buffer()
-            print("{0}\n".format((event['timestamp']*10)%10))
+            #print("{0}\n".format((event['timestamp']*10)%10))
             if (event['timestamp']*10)%10>5:
                 self.g.write(95)
         self.g.state.text_mode_off()
