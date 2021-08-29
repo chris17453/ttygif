951047fb0a3cb08a09c76f166d45d98e6f4dc52a
chris17453 <chris17453@gmail.com>
2021-08-29 13:19:08 -0400

    Bump Version 1.2.561

--- ttygif/tty/theme.pyx
+++ ttygif/tty/theme.pyx
@@ -51,7 +51,7 @@ cdef class layer:
             if frame['image']:
                 attribs=frame['descriptor']
                 # if not set auto set...
-                if self.outer.width==0 and self.outer.height==0 and self.outer.top==0 and self.outer.left==0 and self.outer.right==0 and self.outer.bottom=0:
+                if self.outer.width==0 and self.outer.height==0 and self.outer.top==0 and self.outer.left==0 and self.outer.right==0 and self.outer.bottom==0:
                     self.outer =rect(0,0,attribs.Width-1,attribs.Height-1)
         
                 self.image=image(1,attribs.Width,attribs.Height,array.array('B',gif_raw['global_color_table'].colors),clear_1)
